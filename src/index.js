7d8a141664ef67bc576d1ab4ce40bc7bbd2939e2
HannaYef <pingvinvkedax@gmail.com>
2022-10-13 14:39:38 +0100

    update js

--- src/index.js
+++ src/index.js
@@ -56,6 +56,15 @@ function showWeather(response) {
   console.log(response);
   // let degree = document.querySelector("celsium");
   //degree.innerHTML = `${temperature}`;
+  let humidity = document.querySelector("#humidity");
+  console.log(humidity);
+  humidity.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  console.log(wind);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let descriptionElement = document.querySelector("#description");
+  console.log(description);
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
